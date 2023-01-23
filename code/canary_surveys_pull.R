d1783fba5ca1337257638046abd9708bb2270aed
okenk <kiva.oken@noaa.gov>
2023-01-23 10:41:29 -0800

    prep for data workshop, redo gdrive paths in code

--- code/canary_surveys_pull.R
+++ code/canary_surveys_pull.R
@@ -1,20 +1,21 @@
 library(here)
+library(magrittr)
 
 # catch
-wcgbts_catch <- nwfscSurvey::pull_catch(common_name = 'canary rockfish', 
-                                        survey = 'NWFSC.Combo', 
-                                        dir = here('data'),
-                                        convert = TRUE) %>%
-  dplyr::mutate(Date_formatted = as.character(Date_formatted),
+wcgbts_catch <- nwfscSurvey::PullCatch.fn(Name = 'canary rockfish', 
+                                          SurveyName = 'NWFSC.Combo', 
+                                          Dir = here('data')) %>%
+  dplyr::mutate(Date = as.character(Date),
+                Date_formatted = as.character(Date_formatted),
                 State = dplyr::case_when(Latitude_dd < 42 ~ 'CA',
                                          Latitude_dd < 46.25 ~ 'OR',
                                          TRUE ~ 'WA'))
 
-triennial_catch <- nwfscSurvey::pull_catch(common_name = 'canary rockfish', 
-                                           survey = 'Triennial',
-                                           dir = here('data'),
-                                           convert = TRUE) %>%
-  dplyr::mutate(Date_formatted = as.character(Date_formatted),
+triennial_catch <- nwfscSurvey::PullCatch.fn(Name = 'canary rockfish', 
+                                             SurveyName = 'Triennial',
+                                             Dir = here('data')) %>%
+  dplyr::mutate(Date = as.character(Date),
+                Date_formatted = as.character(Date_formatted),
                 State = dplyr::case_when(Latitude_dd < 42 ~ 'CA',
                                          Latitude_dd < 46.25 ~ 'OR',
                                          TRUE ~ 'WA'))
@@ -24,8 +25,12 @@ triennial_catch <- nwfscSurvey::pull_catch(common_name = 'canary rockfish',
 wcgbts_bio <- nwfscSurvey::pull_bio(common_name = 'canary rockfish', 
                                     survey = 'NWFSC.Combo',
                                     dir = here('data'),
-                                    convert = TRUE)
-wcgbts_bio$Date_formatted <- as.character(wcgbts_bio$Date_formatted)
+                                    convert = TRUE) %>%
+  dplyr::mutate(Data = as.character(Data),
+                Date_formatted = as.character(Date_formatted),
+                State = dplyr::case_when(Latitude_dd < 42 ~ 'CA',
+                                         Latitude_dd < 46.25 ~ 'OR',
+                                         TRUE ~ 'WA'))
 
 triennial_bio <- nwfscSurvey::pull_bio(common_name = 'canary rockfish', 
                                     survey = 'Triennial',
@@ -45,38 +50,38 @@ triennial_bio_samples <- nwfscSurvey::pull_biological_samples(common_name = 'can
                                                            survey = 'NWFSC.Combo')
 
 # Calculate total research catches
-research_catch <- list(NWFSC.Combo = dplyr::select(wcgbts_catch, Year, Total_catch_numbers,
-                                Total_catch_wt_kg, CPUE_kg_km2, State),
-                       Triennial = dplyr::select(triennial_catch, Year, Total_catch_numbers,
-                                                 Total_catch_wt_kg, CPUE_kg_km2, State)) %>%
+research_catch <- list(NWFSC.Combo = dplyr::select(wcgbts_catch, Year, total_catch_numbers,
+                                                   total_catch_wt_kg, cpue_kg_km2, State),
+                       Triennial = dplyr::select(triennial_catch, Year, total_catch_numbers,
+                                                 total_catch_wt_kg, cpue_kg_km2, State)) %>%
   dplyr::bind_rows(.id = 'Survey') %>%
   dplyr::group_by(Year, State, Survey) %>%
-  dplyr::summarise(Total_catch_numbers = sum(Total_catch_numbers),
-                   Total_catch_wt_kg = sum(Total_catch_wt_kg))
+  dplyr::summarise(total_catch_numbers = sum(total_catch_numbers),
+                   total_catch_wt_kg = sum(total_catch_wt_kg))
 
 # upload to gdrive!!!!!
 xx <- googledrive::drive_create(name = 'wcgbts_catch',
-                                path = 'https://drive.google.com/drive/folders/1fleYIaLvdIYMLv14--P1804akQvnWu5J', 
+                                path = 'https://drive.google.com/drive/folders/1Lx4JN-nmJkWtcqmelODZYoVrHyVLzegP', 
                                 type = 'spreadsheet', overwrite = TRUE)
 googlesheets4::sheet_write(wcgbts_catch, ss = xx, sheet = 1)
 
 xx <- googledrive::drive_create(name = 'triennial_catch',
-                                path = 'https://drive.google.com/drive/folders/1fleYIaLvdIYMLv14--P1804akQvnWu5J', 
+                                path = 'https://drive.google.com/drive/folders/1Lx4JN-nmJkWtcqmelODZYoVrHyVLzegP', 
                                 type = 'spreadsheet', overwrite = TRUE)
 googlesheets4::sheet_write(triennial_catch, ss = xx, sheet = 1)
 
 xx <- googledrive::drive_create(name = 'wcgbts_bio',
-                                path = 'https://drive.google.com/drive/folders/1fleYIaLvdIYMLv14--P1804akQvnWu5J', 
+                                path = 'https://drive.google.com/drive/folders/1Lx4JN-nmJkWtcqmelODZYoVrHyVLzegP', 
                                 type = 'spreadsheet', overwrite = TRUE)
 googlesheets4::sheet_write(wcgbts_bio, ss = xx, sheet = 1)
 
 xx <- googledrive::drive_create(name = 'triennial_bio',
-                                path = 'https://drive.google.com/drive/folders/1fleYIaLvdIYMLv14--P1804akQvnWu5J', 
+                                path = 'https://drive.google.com/drive/folders/1Lx4JN-nmJkWtcqmelODZYoVrHyVLzegP', 
                                 type = 'spreadsheet', overwrite = TRUE)
 googlesheets4::sheet_write(triennial_bio$length_data, ss = xx, sheet = 'length')
 googlesheets4::sheet_write(triennial_bio$age_data, ss = xx, sheet = 'age')
 
 xx <- googledrive::drive_create(name = 'research_catch',
-                                path = 'https://drive.google.com/drive/folders/1fleYIaLvdIYMLv14--P1804akQvnWu5J', 
+                                path = 'https://drive.google.com/drive/folders/1Lx4JN-nmJkWtcqmelODZYoVrHyVLzegP', 
                                 type = 'spreadsheet', overwrite = TRUE)
 googlesheets4::sheet_write(research_catch, ss = xx, sheet = 1)
--- code/canary_surveys_workup.R
+++ code/canary_surveys_workup.R
@@ -1,5 +1,9 @@
 # Analyze and plot geographic difference in VBGF curve from survey data
 
+# Construct model-based index
+
+# Explore survey comps
+
 library(magrittr)
 library(ggplot2)
 library(here) 
@@ -7,24 +11,27 @@ theme_set(theme_classic())
 
 # option to load data from disk
 # would be good to not have the dates hard coded in.
-# load(here('data/catch_canary rockfish_NWFSC.Combo_2022-11-04.rda'))
-# wcgbts_catch <- catch
-# load(here('data/catch_canary rockfish_Triennial_2022-11-04.rda'))
-# triennial_catch <- catch
-# load(here('data/bio_canary rockfish_NWFSC.Combo_2022-11-03.rda'))
-# wcgbts_bio <- bio
-# load(here('data/bio_canary rockfish_Triennial_2022-11-03.rda'))
-# triennial_bio <- bio
+load(here('data/catch_canary rockfish_NWFSC.Combo_2023-01-17.rdata'))
+wcgbts_catch <- x
+load(here('data/catch_canary rockfish_Triennial_2023-01-17.rdata'))
+triennial_catch <- catch
+load(here('data/bio_canary rockfish_NWFSC.Combo_2023-01-17.rdata'))
+wcgbts_bio <- x
+load(here('data/bio_canary rockfish_Triennial_2023-01-17.rdata'))
+triennial_bio <- bio
 
-# load data from gdrive
-wcgbts_bio <- googlesheets4::read_sheet(ss = 'https://docs.google.com/spreadsheets/d/1VRGKrehGl2zelBxytpKI7QJWjk8ACPSEGY6VKXLIbpc/edit#gid=0')
-triennial_bio <- googlesheets4::read_sheet(ss = 'https://docs.google.com/spreadsheets/d/10nBQ0pB1gOpEAESCV331THjErRAZ6zqNAAkySMIzjLg/edit#gid=0', 
+load data from gdrive
+wcgbts_bio <- googlesheets4::read_sheet(ss = 'https://docs.google.com/spreadsheets/d/1GfsJ4oJNqXm6tjSA3NeBR9L6F1ZwQp5b7-f7nHMKM2A/edit#gid=0')
+triennial_bio <- googlesheets4::read_sheet(ss = 'https://docs.google.com/spreadsheets/d/1cStwfD0-jaNj44ANCEmNNw7lWrMRVdrmt3GjwiqVfl8/edit#gid=0',
                                            sheet = 'age')
-# Coos Bay latitude: 43.3672
+Coos Bay latitude: 43.3672
 
 # Combine age data from triennial and wcgbts, create flags --------
+
+# Note the A-L models below only use WCGBTS data though
+
 age_combo <- dplyr::select(wcgbts_bio, Year, Length_cm, Sex, Age_years, Latitude_dd, Longitude_dd) %>%
-  dplyr::bind_rows(trawl = ., triennial = dplyr::select(triennial_bio, Year, Length_cm, Sex, Age_years, Latitude_dd, Longitude_dd), 
+  dplyr::bind_rows(trawl = ., triennial = dplyr::select(triennial_bio$age_data, Year, Length_cm, Sex, Age_years, Latitude_dd, Longitude_dd), 
                    .id = 'survey', ) %>%
   dplyr::mutate(is_south_cb = factor(dplyr::if_else(Latitude_dd < 43.3672,
                                         TRUE, FALSE)),
@@ -41,23 +48,24 @@ age_combo <- dplyr::select(wcgbts_bio, Year, Length_cm, Sex, Age_years, Latitude
 
 ### fit coastwide age-length model
 coastwide <- nls(Length_cm ~linf*(1-exp(-k*(Age_years-t0))), data = age_combo, 
-                 start = list(linf = 55, k = 0.3, t0 = 0)) 
+                 start = list(linf = 55, k = 0.3, t0 = 0), subset = survey == 'trawl') 
 
 ### fit age-length model by sex. This is a biologically significant difference
 ### to compare regional differences to
 split_sex <- nls(Length_cm ~ linf[Sex]*(1-exp(-k[Sex]*(Age_years-t0[Sex]))), data = age_combo, 
-      start = list(linf = rep(55,3), k = rep(0.3,3), t0 = rep(0,3))) 
+      start = list(linf = rep(55,3), k = rep(0.3,3), t0 = rep(0,3)), subset = survey == 'trawl') 
 
 ### fit models for three different regional division schemes, compare them
 split_region_ca <- nls(Length_cm ~ linf[is_south_ca]*(1-exp(-k[is_south_ca]*(Age_years-t0[is_south_ca]))), data = age_combo, 
-                       start = list(linf = rep(55,2), k = rep(0.3,2), t0 = rep(0,2))) 
+                       start = list(linf = rep(55,2), k = rep(0.3,2), t0 = rep(0,2)),
+                       subset = survey == 'trawl') 
 
 
 split_region_cb <- nls(Length_cm ~ linf[is_south_cb] * (1-exp(-k[is_south_cb]*(Age_years-t0[is_south_cb]))), data = age_combo, 
-                       start = list(linf = rep(55,2), k = rep(0.3,2), t0 = rep(0,2))) 
+                       start = list(linf = rep(55,2), k = rep(0.3,2), t0 = rep(0,2)), subset = survey == 'trawl') 
 
 split_region_wa <- nls(Length_cm ~ linf[is_south_wa] * (1-exp(-k[is_south_wa]*(Age_years-t0[is_south_wa]))), data = age_combo, 
-                       start = list(linf = rep(55,2), k = rep(0.3,2), t0 = rep(0,2))) 
+                       start = list(linf = rep(55,2), k = rep(0.3,2), t0 = rep(0,2)), subset = survey == 'trawl') 
 
 AIC(split_region_ca, split_region_cb, split_region_wa, coastwide, split_sex)
 
@@ -135,9 +143,10 @@ age_combo %>%
   dplyr::summarise(Pct_female = sum(Sex == 'F') / dplyr::n(),
                    se = sqrt(Pct_female*(1-Pct_female)/dplyr::n())) %>%
  # dplyr::mutate(Age_years = as.numeric(Age_years)) %>%
-  ggplot() +
-  geom_point(aes(x = Age_years, y = Pct_female)) +
-  geom_segment(aes(x = Age_years, y = Pct_female - se, xend = Age_years, yend = Pct_female + se))
+  ggplot(aes(x = Age_years, y = Pct_female)) +
+  geom_point() +
+  geom_segment(aes(y = Pct_female - se, xend = Age_years, yend = Pct_female + se)) +
+  geom_vline(xintercept = 19.5, col = 'red')
 
 
 # Data workshop figures ---------------------------------------------------
@@ -145,12 +154,13 @@ age_combo %>%
 wcgbts_bio %>%
   dplyr::filter(!is.na(Age_years)) %>%
   ggplot(data = ., aes(x = Age_years, y = Length_cm, col = Sex)) +
-  geom_point(alpha = 0.25) +
+  geom_point(alpha = 0.15) +
   labs(x = 'Age (years)', y = 'Length (cm)') +
   scale_color_manual(values = c('F' = 'blue', 'M' = 'red', 'U' = 'darkgoldenrod1'))
   
 ggsave(filename = here('data_workshop_figs/len_data.png'), device = 'png', 
        height = 5, width = 7, units = 'in', dpi = 500)
+
 
 ## Age-length scatter plot with fitted lines by sex-region
 # First get coefficients out of the regression objects in correct format
@@ -235,3 +245,65 @@ wcgbts_ca %>%
   xlim(0,75)
 ggsave(filename = here('data_workshop_figs/wcgbts_lengths.png'), device = 'png', 
        height = 5, width = 9, units = 'in', dpi = 500)
+
+
+# Design-based index -------------------------------------------------------
+
+temp <- wcgbts_catch %>%
+  dplyr::mutate(new = factor(
+    cpue_kg_km2 <= 0,
+    levels = c(FALSE, TRUE),
+    labels = c("Present", "Absent")
+  ))
+nwfscSurvey::plot_proportion(data = temp, column_factor = new, column_bin = 'Depth_m', width = 50)
+with(wcgbts_catch, sum(total_catch_wt_kg > 0 & Depth_m > 350) / sum(total_catch_wt_kg > 0))
+# Choose 350 as cutoff, includes 99.9% of all positive tows. 
+
+wcgbts_catch %>%
+  tibble::as_tibble() %>%
+  dplyr::filter(total_catch_numbers > 0) %>%
+  dplyr::arrange(desc(Depth_m)) %>% View
+# 350m depth excludes 1 fish in 1 tow.
+
+strata <- nwfscSurvey::CreateStrataDF.fn(
+  names = c("shallow_s", "deep_s", "shallow_n", "deep_n"), 
+  depths.shallow = c( 55,   183,  55,   183),
+  depths.deep    = c(183,   300,  183,  300),
+  lats.south     = c( 32,   32,   34.5, 34.5),
+  lats.north     = c( 34.5, 34.5, 49,   49))
+
+biomass <- nwfscSurvey::Biomass.fn(dir = NULL, 
+                                   dat = wcgbts_catch,  
+                                   strat.df = strata)
+ggplot(biomass$Bio,
+       aes(x = as.numeric(Year),
+           y = Value,
+           ymin = exp(log(Value) - 1.96*seLogB),
+           ymax = exp(log(Value) + 1.96*seLogB))) +
+  geom_pointrange() +
+  geom_line() +
+  labs(x = 'Year', y = 'Index')
+
+
+
+
+# Where are the missing ages? ---------------------------------------------
+
+wcgbts_bio %>%
+  dplyr::filter(!is.na(Age_years)) %>%
+  with(max(Year))
+
+nwfsc_data_warehouse <- wcgbts_bio %>%
+  dplyr::group_by(Year) %>%
+  dplyr::summarise(n_lengths = sum(!is.na(Length_cm)),
+                   n_ages = sum(!is.na(Age_years)))
+
+from_patrick <- readr::read_csv(here('data/ageing_from_patrick.csv'), na = c('', '-'))
+
+dplyr::left_join(nwfsc_data_warehouse, dplyr::select(from_patrick, -Unaged)) %>%
+  dplyr::rename(n_data_warehouse = 'n_ages', n_summer_2022 = 'Aged') %>%
+  dplyr::mutate(data_warehouse_undercount = n_summer_2022 - n_data_warehouse) %>%
+  View
+
+
+                 
