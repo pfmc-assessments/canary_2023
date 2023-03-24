0b6018d4fcc54e3e6d99be0330d11f2ba6e18645
okenk <kiva.oken@noaa.gov>
2023-03-24 10:18:16 -0700

    more model runs and document editing

--- code/kiva_model_runs.R
+++ code/kiva_model_runs.R
@@ -7,31 +7,37 @@ r4ss::run(dir = here('models/2015base'), exe = 'ss3.exe', #extras = '-nohess',
 copy_SS_inputs(dir.old = here('models/2015base'), 
                dir.new = here('models/transition'))
 
-data.converted <- SS_readdat(here('models/converted/data.ss'))
-control.converted <- SS_readctl(here('models/converted/control.ss'), datlist = data.converted)
-
-canary.converted <- SS_read(here('models/converted'))
-
-canary.converted$dat$lbin_vector_pop
+converted <- SS_read(here('models/converted'))
 
 # eliminate age comp rows with input sample sizes of zero (From Triennial)
-data.converted$agecomp <- data.converted$agecomp[data.converted$agecomp[,'Nsamp'] > 0,]
+converted$dat$agecomp <- converted$dat$agecomp[converted$dat$agecomp[,'Nsamp'] > 0,]
 
 # prerecruit survey needs to be redefined for 3.30
-data.converted$fleetinfo$units[grep('prerec', data.converted$fleetinfo$fleetname)] <- 32
+converted$dat$fleetinfo$units[grep('prerec', converted$dat$fleetinfo$fleetname)] <- 32
 
-r4ss::SS_writedat(data.converted, outfile = here('models/converted/data.ss'), 
+r4ss::SS_writedat(converted$dat, outfile = here('models/converted/data.ss'), 
                   overwrite = TRUE)
 
 # prerecruit survey needs to be redefined for 3.30
-control.converted$size_selex_types$Pattern <- sapply(control.converted$size_selex_types$Pattern, 
+converted$ctl$size_selex_types$Pattern <- sapply(converted$ctl$size_selex_types$Pattern, 
                                                      function(x) ifelse(x == 32, 0, x))
 
-r4ss::SS_writectl(control.converted, outfile = here('models/converted/control.ss'), 
-                  overwrite = TRUE)
+# Change fishing mortality to year-round, gets rid of ss3 warnings
+converted$dat$fleetinfo$surveytiming[converted$dat$fleetinfo$type==1] <- -1
 
-r4ss::run(dir = here('models/converted'), exe = 'ss_win.exe', #extras = '-nohess', 
-          show_in_console = FALSE, skipfinished = FALSE)
+# per warnings file "simpler and takes 1 parm for each settlement"
+converted$ctl$recr_dist_method <- 3
+# This messes up the ctl file, but I can't figure out what is wrong.
+
+r4ss::SS_write(converted, 
+               dir = here('models/converted_rec_dist3'), 
+               overwrite = TRUE)
+
+r4ss::run(dir = here('models/converted_rec_dist3'), 
+          exe = here('models/converted/ss_win.exe'), 
+          extras = '-nohess', 
+          show_in_console = TRUE, 
+          skipfinished = FALSE)
 
 xx <- SSgetoutput(dirvec = glue::glue("{models}/{subdir}", models = here('models'),
                                 subdir = c('2015base', 'converted')))
