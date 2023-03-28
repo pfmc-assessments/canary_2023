5000c893628a12f90528eb8cc24a3a7ab15b47d1
brianlangseth-NOAA <brian.langseth@NOAA.gov>
2023-03-28 09:56:23 -0700

    Upload PacFIN length comp data and sample sizes (#32)

--- code/canary_compositions.R
+++ code/canary_compositions.R
@@ -18,7 +18,7 @@ if(Sys.getenv("USERNAME") == "Brian.Langseth") {
 }
 
 ################################
-#Load PacFIN BDS data
+#Load PacFIN BDS data and set up data
 ################################
 load(file.path(dir, "PacFIN.CNRY.bds.01.Sep.2022.RData"))
 pacfin <- bds.pacfin
@@ -35,25 +35,46 @@ ub = wlcoef[wlcoef$Sex=="U","B"]
 # Read in the PacFIN catch data to based expansion on
 catch.file <- data.frame(googlesheets4::read_sheet('https://docs.google.com/spreadsheets/d/17x0PT_vqTv1kvHAwaqgz7jmKvCvRwm7OcFb4jrh_AWo/edit#gid=2086044691',
                                                    sheet = "catch_mt"))
-colnames(catch.file)[1] = c("Year")
+colnames(catch.file)[1] = c("year")
 
 # Clean up length data
-pacfin[pacfin$FISH_LENGTH_UNITS %in% "UNK", "FISH_LENGTH_UNITS"] = "CM" #these are CMs
+pacfin[pacfin$FISH_LENGTH_UNITS %in% "UNK", "FISH_LENGTH_UNITS"] = "CM" #these are CMs - need to do first so cleanPacFIN runs
 Pdata = cleanPacFIN(Pdata = pacfin, 
                     CLEAN = TRUE,
                     keep_length_type = c("", "F", "A", "U", NA), #removes the 2 standard length samples
-                    keep_sample_type = c("M","S"), #keep special project samples
+                    keep_sample_type = c("M","S"), #keep ALL special project samples
                     verbose = TRUE)
-Pdata <- Pdata[!(Pdata$SAMPLE_TYPE %in% c("S") & Pdata$SAMPLE_YEAR>1986),] #Keep oregon special project data before and including 1986
+Pdata <- Pdata[!(Pdata$SAMPLE_TYPE %in% c("S") & Pdata$SAMPLE_YEAR>1986),] #Filter out oregon special project data after 1986
 
+#Convert geargrouping based on fleet structure - HKL, MSC, NET, TLS as "NTWL"; TWL and TWS as "TWL"
+table(pacfin$AGENCY_GEAR_CODE,pacfin$PACFIN_GEAR_CODE)
+table(Pdata$GEAR,Pdata$geargroup)
+fleet <- dplyr::case_when(Pdata$geargroup %in% c("HKL","MSC","NET","TLS") ~ "NTWL",
+                                Pdata$geargroup %in% c("TWL","TWS") ~ "TWL")
+state <- dplyr::case_when(Pdata$state == "WA" ~ "W",
+                          Pdata$state == "OR" ~ "O",
+                          Pdata$state == "CA" ~ "C")
+Pdata$fleet <- paste(fleet, state, sep = ".")
 
-##<<<<<<<<<<<<<<<<<<<CONTINUE HERE
-Pdata$fleet = Pdata$state
-Pdata$stratification = Pdata$fleet
+#Pdata$stratification = Pdata$fleet #expansion function now allows setting of stratification
 
 PdataAge = Pdata #set up for age comps later
 
 Pdata <- Pdata[!is.na(Pdata[, 'length']),] #Remove fish without lengths. Do this here because some of these (11) have ages
+
+
+#################################################################################
+# Length samples and trips by area and fleet
+#################################################################################
+
+trips_sample <- Pdata %>%
+  dplyr::group_by(fleet, year) %>%
+  dplyr::summarise(
+    Trips = length(unique(SAMPLE_NO)),
+    Lengths = length(lengthcm)
+  )
+colnames(trips_sample)[2] <- "Year"
+write.csv(trips_sample, row.names = FALSE, file = file.path(git_dir, "data", "Canary_PacFIN_LengthComps_trips_and_samples.csv"))
 
 
 #################################################################################
@@ -66,28 +87,79 @@ Pdata_exp <- getExpansion_1(Pdata = Pdata,
 Pdata_exp <- getExpansion_2(Pdata = Pdata_exp, 
                             Catch = catch.file, 
                             Units = "MT",
-                            maxExp = 0.80)
+                            maxExp = 0.95,
+                            stratification.cols = "fleet")
 
 Pdata_exp$Final_Sample_Size <- capValues(Pdata_exp$Expansion_Factor_1_L * Pdata_exp$Expansion_Factor_2, maxVal = 0.80)
 
 # Set up lengths bins based on length sizes for all comps
-myLbins = c(seq(10, 50, 2))
-
-calcSamplesL() #run function (at end) to report and save sample sizes by sex
-
-# Since quillback is a single sex model I am going to change all fish to be unsexed.
-Pdata_exp$SEX = "U"
+myLbins = c(seq(12, 66, 2))
 
 Lcomps = getComps(Pdata_exp, Comps = "LEN")
 
 writeComps(inComps = Lcomps, 
-           fname = file.path(dir, "forSS", "Lcomps.QLBK.Feb.2021.csv"), 
+           fname = file.path(git_dir, "data", "forSS", "Canary_PacFIN_LengthComps.csv"), 
            lbins = myLbins, 
-           partition = 0, 
+           partition = 2, 
            sum1 = TRUE,
            digits = 4)
 
 
+##############################################################################################################
+# Format and rewrite
+##############################################################################################################
+
+#Have to use header = FALSE here because when TRUE I cant read each set of comps, and the variable
+#names are fixed as the ones for the combined comps
+out = read.csv(file.path(git_dir, "data", "forSS", "Canary_PacFIN_LengthComps.csv"), skip = 3, header = FALSE)
+
+##
+#Extract Unsexed fish
+##
+start = which(as.character(out[,1]) %in% c(" Usexed only ")) + 2
+end   = nrow(out)
+cut_out = out[start:end,]
+colnames(cut_out) <- out[start-1,]
+
+ind = which(colnames(cut_out) %in% "U12"):which(colnames(cut_out) %in% "U.66") #For 2 sex model need to go to U.66
+format = cbind(cut_out$fleet, cut_out$year, cut_out$month, cut_out$fleet, cut_out$sex, cut_out$partition, 
+               cut_out$Ntows, cut_out$Nsamps, cut_out$InputN, cut_out[,ind])
+colnames(format) = c("state", "fishyr", "month", "fleet", "sex", "part", "Ntows", "Nsamps", "InputN", colnames(cut_out[ind]))
+
+format$state <- sub("^.*\\.","", format$state) #keep only stuff after "."
+format$fleet <- sub("\\..*", "", format$fleet) #keep only stuff before "."
+
+ca_comps = format[format$state == "C", ]
+or_comps = format[format$state == "O", ]
+wa_comps = format[format$state == "W", ]
+
+# write.csv(ca_comps, file = file.path(git_dir, "data", "forSS","CA_PacFIN_Lcomps_unsexed_12_66_formatted.csv"), row.names = FALSE)
+# write.csv(or_comps, file = file.path(git_dir, "data", "forSS","OR_PacFIN_Lcomps_unsexed_12_66_formatted.csv"), row.names = FALSE)
+# write.csv(wa_comps, file = file.path(git_dir, "data", "forSS","WA_PacFIN_Lcomps_unsexed_12_66_formatted.csv"), row.names = FALSE)
+
+##
+#Extract sexed fish
+##
+start = 1 + 1
+end   = which(as.character(out[,1]) %in% c(" Females only "))
+cut_out = out[start:end,]
+colnames(cut_out) <- out[1,]
+
+ind = which(colnames(cut_out) %in% "F12"):which(colnames(cut_out) %in% "M66")
+format = cbind(cut_out$fleet, cut_out$year, cut_out$month, cut_out$fleet, cut_out$sex, cut_out$partition, 
+               cut_out$Ntows, cut_out$Nsamps, cut_out$InputN, cut_out[,ind])
+colnames(format) = c("state", "fishyr", "month", "fleet", "sex", "part", "Ntows", "Nsamps", "InputN", colnames(cut_out[ind]))
+
+format$state <- sub("^.*\\.","", format$state) #keep only stuff after "."
+format$fleet <- sub("\\..*", "", format$fleet) #keep only stuff before "."
+
+ca_comps = format[format$state == "C", ]
+or_comps = format[format$state == "O", ]
+wa_comps = format[format$state == "W", ]
+
+# write.csv(ca_comps, file = file.path(git_dir, "data", "forSS","CA_PacFIN_Lcomps_Sexed_12_66_formatted.csv"), row.names = FALSE)
+# write.csv(or_comps, file = file.path(git_dir, "data", "forSS","OR_PacFIN_Lcomps_Sexed_12_66_formatted.csv"), row.names = FALSE)
+# write.csv(wa_comps, file = file.path(git_dir, "data", "forSS","WA_PacFIN_Lcomps_Sexed_12_66_formatted.csv"), row.names = FALSE)
 
 
 
