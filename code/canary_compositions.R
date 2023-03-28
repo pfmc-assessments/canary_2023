e528ba51e5a1d112c051cb0d50c2325ebbc5b98a
brianlangseth-NOAA <brian.langseth@NOAA.gov>
2023-03-28 11:45:48 -0700

    Set partition to 0, add coastal expanded comps (#32)

--- code/canary_compositions.R
+++ code/canary_compositions.R
@@ -56,11 +56,12 @@ state <- dplyr::case_when(Pdata$state == "WA" ~ "W",
                           Pdata$state == "CA" ~ "C")
 Pdata$fleet <- paste(fleet, state, sep = ".")
 
-#Pdata$stratification = Pdata$fleet #expansion function now allows setting of stratification
-
 PdataAge = Pdata #set up for age comps later
 
 Pdata <- Pdata[!is.na(Pdata[, 'length']),] #Remove fish without lengths. Do this here because some of these (11) have ages
+
+# PdataCoast <- Pdata #set up coast length comps for later
+# PdataCoast$fleet <- sub("\\..*", "", PdataCoast$fleet) #keep only stuff before "."
 
 
 #################################################################################
@@ -74,7 +75,8 @@ trips_sample <- Pdata %>%
     Lengths = length(lengthcm)
   )
 colnames(trips_sample)[2] <- "Year"
-write.csv(trips_sample, row.names = FALSE, file = file.path(git_dir, "data", "Canary_PacFIN_LengthComps_trips_and_samples.csv"))
+# write.csv(trips_sample, row.names = FALSE, file = file.path(git_dir, "data", "Canary_PacFIN_LengthComps_trips_and_samples.csv"))
+#Coast samples we can sum together
 
 
 #################################################################################
@@ -100,10 +102,37 @@ Lcomps = getComps(Pdata_exp, Comps = "LEN")
 writeComps(inComps = Lcomps, 
            fname = file.path(git_dir, "data", "forSS", "Canary_PacFIN_LengthComps.csv"), 
            lbins = myLbins, 
-           partition = 2, 
+           partition = 0, 
            sum1 = TRUE,
            digits = 4)
 
+# ##
+# #Coastal expansion
+# ##
+# PdataCoast_exp <- getExpansion_1(Pdata = PdataCoast,
+#                             fa = fa, fb = fb, ma = ma, mb = mb, ua = ua, ub = ub)
+# 
+# PdataCoast_exp <- getExpansion_2(Pdata = PdataCoast_exp, 
+#                             Catch = data.frame("year" = catch.file$year, 
+#                                                "NTWL" = rowSums(catch.file[,c("NTWL.C","NTWL.O","NTWL.W")],na.rm=T), 
+#                                                "TWL" = rowSums(catch.file[,c("TWL.C","TWL.O","TWL.W")],na.rm=T)), 
+#                             Units = "MT",
+#                             maxExp = 0.95,
+#                             stratification.cols = "fleet")
+# 
+# PdataCoast_exp$Final_Sample_Size <- capValues(PdataCoast_exp$Expansion_Factor_1_L * PdataCoast_exp$Expansion_Factor_2, maxVal = 0.80)
+# 
+# # Set up lengths bins based on length sizes for all comps
+# myLbins = c(seq(12, 66, 2))
+# 
+# LcompsCoast = getComps(PdataCoast_exp, Comps = "LEN")
+# 
+# writeComps(inComps = LcompsCoast, 
+#            fname = file.path(git_dir, "data", "forSS", "Canary_PacFIN_Coastal_LengthComps.csv"), 
+#            lbins = myLbins, 
+#            partition = 0, 
+#            sum1 = TRUE,
+#            digits = 4)
 
 ##############################################################################################################
 # Format and rewrite
@@ -161,6 +190,41 @@ wa_comps = format[format$state == "W", ]
 # write.csv(or_comps, file = file.path(git_dir, "data", "forSS","OR_PacFIN_Lcomps_Sexed_12_66_formatted.csv"), row.names = FALSE)
 # write.csv(wa_comps, file = file.path(git_dir, "data", "forSS","WA_PacFIN_Lcomps_Sexed_12_66_formatted.csv"), row.names = FALSE)
 
+
+# ##
+# #Coastal expansion
+# ##
+# out = read.csv(file.path(git_dir, "data", "forSS", "Canary_PacFIN_Coastal_LengthComps.csv"), skip = 3, header = FALSE)
+# 
+# ##Extract Unsexed fish
+# start = which(as.character(out[,1]) %in% c(" Usexed only ")) + 2
+# end   = nrow(out)
+# cut_out = out[start:end,]
+# colnames(cut_out) <- out[start-1,]
+# 
+# ind = which(colnames(cut_out) %in% "U12"):which(colnames(cut_out) %in% "U.66") #For 2 sex model need to go to U.66
+# format = cbind(cut_out$fleet, cut_out$year, cut_out$month, cut_out$fleet, cut_out$sex, cut_out$partition, 
+#                cut_out$Ntows, cut_out$Nsamps, cut_out$InputN, cut_out[,ind])
+# colnames(format) = c("state", "fishyr", "month", "fleet", "sex", "part", "Ntows", "Nsamps", "InputN", colnames(cut_out[ind]))
+# 
+# format$state <- "coastal"
+# 
+# # write.csv(format, file = file.path(git_dir, "data", "forSS","Coastal_PacFIN_Lcomps_unsexed_12_66_formatted.csv"), row.names = FALSE)
+# 
+# ##Extract sexed fish
+# start = 1 + 1
+# end   = which(as.character(out[,1]) %in% c(" Females only "))
+# cut_out = out[start:end,]
+# colnames(cut_out) <- out[1,]
+# 
+# ind = which(colnames(cut_out) %in% "F12"):which(colnames(cut_out) %in% "M66")
+# format = cbind(cut_out$fleet, cut_out$year, cut_out$month, cut_out$fleet, cut_out$sex, cut_out$partition, 
+#                cut_out$Ntows, cut_out$Nsamps, cut_out$InputN, cut_out[,ind])
+# colnames(format) = c("state", "fishyr", "month", "fleet", "sex", "part", "Ntows", "Nsamps", "InputN", colnames(cut_out[ind]))
+# 
+# format$state <- "coastal"
+# 
+# # write.csv(ca_comps, file = file.path(git_dir, "data", "forSS","Coastal_PacFIN_Lcomps_Sexed_12_66_formatted.csv"), row.names = FALSE)
 
 
 
