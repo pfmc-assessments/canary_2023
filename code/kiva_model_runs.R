f3eeae57a20bb054bda28eaa113eb5c236d713a3
okenk <kiva.oken@noaa.gov>
2023-03-29 16:55:16 -0700

    working on comps, document edits

--- code/kiva_model_runs.R
+++ code/kiva_model_runs.R
@@ -22,7 +22,7 @@ r4ss::SS_writedat(converted$dat, outfile = here('models/converted/data.ss'),
 
 # prerecruit survey needs to be redefined for 3.30
 converted$ctl$size_selex_types$Pattern <- sapply(converted$ctl$size_selex_types$Pattern, 
-                                                     function(x) ifelse(x == 32, 0, x))
+                                                 function(x) ifelse(x == 32, 0, x))
 
 # Change fishing mortality to year-round, gets rid of ss3 warnings
 converted$dat$fleetinfo$surveytiming[converted$dat$fleetinfo$type==1] <- -1
@@ -42,7 +42,7 @@ r4ss::run(dir = here('models/converted_rec_dist3'),
           skipfinished = FALSE)
 
 xx <- SSgetoutput(dirvec = glue::glue("{models}/{subdir}", models = here('models'),
-                                subdir = c('2015base', 'converted')))
+                                      subdir = c('2015base', 'converted')))
 
 SSsummarize(xx) |>
   SSplotComparisons(legendlabels = c('2015', 'update SS3 version'))
@@ -69,7 +69,7 @@ r4ss::run(dir = here('models/no_research_catch'),
           skipfinished = FALSE)
 
 SSgetoutput(dirvec = glue::glue("{models}/{subdir}", models = here('models'),
-                                      subdir = c('2015base', 'converted', 'no_research_catch'))) |>
+                                subdir = c('2015base', 'converted', 'no_research_catch'))) |>
   SSsummarize() |>
   SSplotComparisons(legendlabels = c('2015', 'update SS3 version', 'no research catch'))
 # Differences are generally not discernible.
@@ -154,8 +154,9 @@ update.catch$dat$endyr <- 2022
 catches <- read.csv(here('data/canary_total_removals.csv')) 
 
 fleet.converter <- update.catch$dat$fleetinfo |>
-  dplyr::mutate(fleet_no_num = stringr::str_remove(fleetname, '[:digit:]+_')) |>
-  dplyr::select(fleetname, fleet_no_num)
+  dplyr::mutate(fleet_no_num = stringr::str_remove(fleetname, '[:digit:]+_'),
+                fleet = as.numeric(stringr::str_extract(fleetname, '[:digit:]+'))) |>
+  dplyr::select(fleetname, fleet_no_num, fleet)
 
 updated.catch.df <- catches |>
   dplyr::select(-rec.W.N) |>
@@ -168,8 +169,7 @@ updated.catch.df <- catches |>
                                          state == 'C' ~ 'CA'),
                 fleet_no_num = paste(state, gear, sep = '_')) |>
   dplyr::left_join(fleet.converter) |>
-  dplyr::mutate(fleet = as.numeric(stringr::str_extract(fleetname, '[:digit:]+')),
-                seas = 1, 
+  dplyr::mutate(seas = 1, 
                 catch_se = 0.01) |>
   dplyr::select(year = Year, seas, fleet, catch, catch_se) |>
   rbind(c(-999, 1, 1, 0, 0.01)) |>
@@ -198,23 +198,108 @@ SSgetoutput(dirvec = glue::glue("{models}/{subdir}", models = here('models'),
 
 # extend wcgbts ------------------------------------------------------------
 
-update.catch$dat$CPUE |> head()
+r4ss::copy_SS_inputs(dir.old = here('models/update_catch'),
+                     dir.new = here('models/update_wcgbts'), overwrite = TRUE)
+update.wcgbts <- r4ss::SS_read(dir = here('models/update_wcgbts'))
 
-wcgbts.cpue <- all_indices |>
+wcgbts.cpue <- read.csv(here('data/wcgbts_index.csv')) |>
   dplyr::mutate(area = ifelse(area == 'coastwide',
-                       area,
-                       stringr::str_to_upper(area)),
+                              area,
+                              stringr::str_to_upper(area)),
                 fleet_no_num = paste0(area, '_NWFSC')) |>
   dplyr::left_join(fleet.converter) |> 
-  dplyr::mutate(index = as.numeric(stringr::str_extract(fleetname, '[:digit:]+')),
-                seas = 7) |>
-  dplyr::select(year = Year, seas, index, obs = est, se_log = se)
-           
-update.catch$dat$CPUE <- update.catch$dat$CPUE |>
+  dplyr::mutate(seas = 7) |>
+  dplyr::select(year = Year, seas, index = fleet, obs = est, se_log = se)
+
+update.wcgbts$dat$CPUE <- update.wcgbts$dat$CPUE |>
   dplyr::filter(!(index %in% wcgbts.cpue$index)) |>
   dplyr::bind_rows(wcgbts.cpue) |>
   dplyr::arrange(index, year) 
 
+SS_write(update.wcgbts,
+         dir = here('models/update_wcgbts'), 
+         overwrite = TRUE)
+
+r4ss::run(dir = here('models/update_wcgbts'), 
+          exe = here('models/ss_win.exe'), 
+          extras = '-nohess', 
+          # show_in_console = TRUE, 
+          skipfinished = FALSE)
+beepr::beep()
+
+SSgetoutput(dirvec = glue::glue("{models}/{subdir}", models = here('models'),
+                                subdir = c('no_research_catch',  'updateM', 'update_catch', 'update_wcgbts'))) |>
+  SSsummarize() |>
+  SSplotComparisons(legendlabels = c('no research catch', 'update M', 'extend catch', 'extend wcgbts'),
+                    subplots = c(9,3,1))
+
+# Update wcgbts comps -----------------------------------------------------
+r4ss::copy_SS_inputs(dir.old = here('models/update_catch'),
+                     dir.new = here('models/update_wcgbts_comps'), overwrite = TRUE)
+update.wcgbts <- r4ss::SS_read(dir = here('models/update_wcgbts_comps'))
+
+
+length.min <- min(update.wcgbts$dat$lbin_vector)
+length.max <- max(update.wcgbts$dat$lbin_vector)
+age.min <- min(update.wcgbts$dat$agebin_vector)
+age.max <- max(update.wcgbts$dat$agebin_vector)
+
+caal <- list()
+for(ii in 1:4) {
+  area <- c('CA', 'OR', 'WA', 'coastwide')[ii]
+  caal[[area]] <- purrr::map(list(`F` = 'Female', M = 'Male'), function(.x) {
+    out <- read.csv(here(glue::glue('data/{area}_wcgbts_comps/Survey_CAAL_{sex}_Bins_{lmin}_{lmax}_{amin}_{amax}.csv',
+                                    area = area,
+                                    sex = .x,
+                                    lmin = length.min,
+                                    lmax = length.max,
+                                    amin = age.min,
+                                    amax = age.max)))
+    out$Fleet <- fleet.converter$fleet[grep(x = fleet.converter$fleet_no_num,
+                                            pattern = paste0(area, '_NWFSC'))]
+    names(out) <- names(update.wcgbts$dat$agecomp)
+    return(out)
+  })
+}
+
+# need to add marginal ages as -surveys
+
+caal.dfr <- caal |>
+  purrr::list_flatten() |>
+  purrr::list_rbind() |> 
+  dplyr::mutate(dplyr::across(f1:f35, ~ ifelse(Gender == 1, .x, 0)),
+                dplyr::across(m1:m35, ~ ifelse(Gender == 2, .x, 0))) |> # checked row sums, this works
+  dplyr::filter(FltSvy != 28, Lbin_lo > 0) |> # This is not a good long-term thing to do!
+  # caal table is in absolute length, update.wcgbts is in length index.
+  # Updating caal to be in length index
+  dplyr::mutate(dplyr::across(Lbin_lo:Lbin_hi, ~ match(.x, update.wcgbts$dat$lbin_vector))) 
+
+update.wcgbts$dat$agecomp <- update.wcgbts$dat$agecomp |> 
+  dplyr::filter(!(FltSvy %in% unique(caal.dfr$FltSvy))) |>
+  dplyr::bind_rows(caal.dfr)
+
+SS_write(update.wcgbts,
+         dir = here('models/update_wcgbts_comps'), 
+         overwrite = TRUE)
+
+r4ss::run(dir = here('models/update_wcgbts_comps'), 
+          exe = here('models/ss_win.exe'), 
+          extras = '-nohess', 
+          # show_in_console = TRUE, 
+          skipfinished = FALSE)
+beepr::beep()
+
+SSgetoutput(dirvec = glue::glue("{models}/{subdir}", models = here('models'),
+                                subdir = c('no_research_catch',  'updateM', 'update_catch', 
+                                           'update_wcgbts', 'update_wcgbts_comps'))) |>
+  SSsummarize() |>
+  SSplotComparisons(legendlabels = c('no research catch', 'update M', 'update catch', 
+                                     'update wcgbts', 'update wcgbts comps'),
+                    subplots = c(9,3,1))
+
+# survey alone did not move anything!
+# 2015 comps have age comps in length bins that I do not think were observed! 
+# At least not observed and aged.
 
 # No triennial
 # Triennial combined
--- code/surveys_ss3.r
+++ code/surveys_ss3.r
@@ -1,9 +1,13 @@
-# This generates design-based age and length comps for surveys and formats them for SS3
+# This generates design-based age, length, CAAL comps for surveys and formats them for SS3
+# Also writes index results to csv files
 
+# load packages
 library(magrittr)
 library(ggplot2)
-library(here) 
+library(here)
+library(nwfscSurvey)
 
+# load survey data
 load(here('data-raw/Catch__NWFSC.Combo_2023-02-13.rda'))
 wcgbts_catch <- Out
 load(here('data-raw/Catch__Triennial_2023-01-23.rda'))
@@ -13,73 +17,133 @@ wcgbts_bio <- Data
 load(here('data-raw/Bio_All_Triennial_2023-01-23.rda'))
 triennial_bio <- Data
 
+# define age and length bins
 age_bins <- 1:35
 length_bins <- seq(12, 66, by = 2)
 
-# WCGBTS ------------------------------------------------------------------
+# define strata for wcgbts, triennial surveys
+wcgbts_strata <- nwfscSurvey::CreateStrataDF.fn(
+  names = apply(expand.grid(x = c('s.ca', 'n.ca', 'or', 'wa'), 
+                            y = c('deep', 'shallow')), 
+                MARGIN = 1, 
+                FUN = stringr::str_flatten,
+                collapse = '.'),
+  depths.shallow = rep(c(55, 183), each = 4),
+  depths.deep    = rep(c(183, 350), each = 4),
+  lats.south     = rep(c(32, 34.5, 42, 46), 2),
+  lats.north     = rep(c(34.5, 42, 46, 49), 2)
+)
 
-strata <- nwfscSurvey::CreateStrataDF.fn(
-  names = c("shallow_s", "deep_s", "shallow_n", "deep_n"), 
-  depths.shallow = c( 55,   183,  55,   183),
-  depths.deep    = c(183,   350,  183,  350),
-  lats.south     = c( 32,   32,   34.5, 34.5),
-  lats.north     = c( 34.5, 34.5, 49,   49))
+tri_strata <- nwfscSurvey::CreateStrataDF.fn(
+  names = apply(expand.grid(x = c('ca', 'or', 'wa'), 
+                            y = c('deep', 'shallow')), 
+                MARGIN = 1, 
+                FUN = stringr::str_flatten,
+                collapse = '.'),
+  depths.shallow = rep(c(55, 183), each = 3),
+  depths.deep    = rep(c(183, 350), each = 3),
+  lats.south     = rep(c(37, 42, 46), 2), # sampling effort changed btw pt. concep & 37 degrees
+  lats.north     = rep(c(42, 46, 49), 2)
+)
 
+state_border_s <- c(32, 42, 46)
+state_border_n <- c(42, 46, 49)
 
-caal <- SurveyAgeAtLen.fn(dir = here('Data'), 
-                          datAL = wcgbts_bio, datTows = wcgbts_catch, 
-                          strat.df = strata, ageBins = age_bins,
-                          lgthBins = length_bins,
-                          raw = TRUE)
+# Giant for loop to create age, length, caal comps for each state and coastwide for both surveys
+for(survey in 1:2) {
+  if(survey == 1) { # wcgbts
+    catch <- wcgbts_catch
+    bio <- wcgbts_bio
+    strata <- wcgbts_strata
+  } else { # triennial
+    catch <- triennial_catch
+    bio <- triennial_bio
+    strata <- tri_strata
+  }
+  
+  for(ii in 1:4) {
+    # First subset survey data for wcgbts and triennial
+    if(ii < 4) { # state comps
+      if(survey == 1) { # wcgbts
+        catch_subset <- dplyr::filter(catch, 
+                                      Latitude_dd > state_border_s[ii],
+                                      Latitude_dd < state_border_n[ii])
+        bio_subset <- dplyr::filter(bio, 
+                                    Latitude_dd > state_border_s[ii],
+                                    Latitude_dd < state_border_n[ii])
+        ages <- lengths <- bio_subset
+      } else { # tri
+        catch_subset <- dplyr::filter(catch, 
+                                      Latitude_dd > strata$Latitude_dd.1[ii],
+                                      Latitude_dd < state_border_n[ii])
+        bio_subset <- purrr::map(bio, 
+                                 ~ dplyr::filter(.,  
+                                                 Latitude_dd > strata$Latitude_dd.1[ii],
+                                                 Latitude_dd < state_border_n[ii])
+        )  
+        ages <- bio_subset$Ages
+        lengths <- bio_subset$Lengths
+      }
+    } else { # coastwide comps
+      if(survey == 1) { # wcgbts
+        catch_subset <- catch 
+        ages <- lengths <- bio
+      } else { # tri
+        catch_subset <- dplyr::filter(catch, Latitude_dd > 37)
+        bio_subset <- purrr::map(bio, 
+                                 ~ dplyr::filter(., Latitude_dd > 37)
+        )
+        ages <- bio_subset$Ages
+        lengths <- bio_subset$Lengths
+      }
+    }
+    # output  
+    printfolder <- paste(c('CA', 'OR', 'WA', 'coastwide')[ii],
+                         c('wcgbts', 'tri')[survey],
+                         'comps', sep = '_')
+    
+    # now calculate comps
+    caal <- SurveyAgeAtLen.fn(dir = here('Data'), 
+                              datAL = ages, datTows = catch_subset, 
+                              strat.df = strata, ageBins = age_bins,
+                              lgthBins = length_bins,
+                              raw = TRUE,
+                              printfolder = printfolder,
+                              month = '7', ageErr = '1',
+                              verbose = FALSE)
+    
+    for(sex in 1:2){
+      length_n <- GetN.fn(dat = lengths, 
+                          type = 'length')
+      length_freq <- SurveyLFs.fn(dir = here('Data'), 
+                                  datL = lengths, 
+                                  datTows = catch_subset, 
+                                  strat.df = strata, 
+                                  nSamps = length_n,
+                                  lgthBins = length_bins, 
+                                  sex = sex, 
+                                  month = '7',
+                                  verbose = FALSE,
+                                  printfolder = printfolder)
+      
+      age_n <- length.n <- GetN.fn(dat = ages, 
+                                   type = 'age')
+      age_freq <- SurveyAFs.fn(dir = here('data'), 
+                               datA = ages,
+                               datTows = catch_subset,
+                               strat.df = strata, 
+                               ageBins = age_bins, 
+                               nSamps = age_n,
+                               sex = sex,
+                               printfolder = printfolder,
+                               month = '7', ageErr = '1',
+                               verbose = FALSE)
+      
+    }
+  }
+}
+# whew!
 
-
-length_n <- GetN.fn(dat = wcgbts_bio, 
-                    type = 'length')
-wcgbts_length_freq <- SurveyLFs.fn(dir = here('Data'), 
-                                   datL = wcgbts_bio, 
-                                   datTows = wcgbts_catch, 
-                                   strat.df = strata, 
-                                   nSamps = length_n,
-                                   lgthBins = length_bins, 
-                                   sex = 3)
-
-age_n <- length.n <- GetN.fn(dat = wcgbts_bio, 
-                             type = 'age')
-wcgbts_age_freq <- SurveyAFs.fn(dir = here('data'), 
-                                datA = wcgbts_bio,
-                                datTows = wcgbts_catch,
-                                strat.df = strata, 
-                                ageBins = age_bins, 
-                                nSamps = age_n)
-
+# Write index to csv
 load(here('data-raw/NWFSC.Combo/index/lognormal/sdmTMB_save.Rdata'))
 write.csv(all_indices, here('data/wcgbts_index.csv'), row.names = FALSE)
-
-# Triennial ---------------------------------------------------------------
-
-# I believe even if triennial is split, the comps can be done together.
-# Just need to include maximum number of strata
-
-# Are there strata for the triennial?
-
-length_n <- GetN.fn(dat = triennial_bio$Lengths, 
-                    type = 'length')
-tri_length_freq <- SurveyLFs.fn(dir = here('Data'), 
-                                   datL = triennial_bio$Lengths, 
-                                   datTows = triennial_catch, 
-                                   strat.df = strata, 
-                                   nSamps = length_n,
-                                   lgthBins = length_bins, 
-                                   sex = 3)
-
-age_n <- length.n <- GetN.fn(dat = triennial_bio$Ages, 
-                             type = 'age')
-tri_age_freq <- SurveyAFs.fn(dir = here('data'), 
-                             datA = triennial_bio$Ages,
-                             datTows = triennial_catch,
-                             strat.df = strata, 
-                             ageBins = age_bins, 
-                             nSamps = age_n)
-
-
-
