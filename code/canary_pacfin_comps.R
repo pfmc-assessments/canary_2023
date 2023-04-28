c23e7a41416237705dea2c58c15e83efb9d593f7
brianlangseth-NOAA <brian.langseth@NOAA.gov>
2023-04-28 14:57:34 -0700

    Add ager to double reads #26

--- code/canary_pacfin_comps.R
+++ code/canary_pacfin_comps.R
@@ -60,6 +60,9 @@ Pdata$fleet <- paste(fleet, state, sep = ".")
 
   #---------------------------------------------------------------------
   #Pull out WDFW aged fish that have multiple reads of B&B to use for ageing error
+  #Cleaned data exclude reader info, so join based on SAMPLE_NUMBER and FISH_SEQUENCE_NUMBER
+  #from original data and SAMPLE_NO and FISH_NO in cleaned data. These are not unique for CA
+  #but are within WA and OR data
   wa_dReads <- Pdata %>% 
     dplyr::filter(state=="WA" & !(is.na(age1) & is.na(age2) & is.na(age3))) %>% 
     dplyr::mutate(mult = dplyr::case_when(
@@ -68,7 +71,10 @@ Pdata$fleet <- paste(fleet, state, sep = ".")
       (!is.na(age2) & !is.na(age3)) == TRUE ~ 2,
       (!is.na(age1) & !is.na(age3)) == TRUE ~ 2,
       TRUE ~ 1)) %>%
-    dplyr::filter(mult > 1 & !(AGE_METHOD1 %in% c("S") & mult == 2))
+    dplyr::filter(mult > 1 & !(AGE_METHOD1 %in% c("S") & mult == 2)) %>%
+    dplyr::left_join(.,pacfin[pacfin$AGENCY_CODE=="W",
+                              c("SAMPLE_NUMBER","FISH_SEQUENCE_NUMBER","agedby1","agedby2","agedby3")],
+                     by = dplyr::join_by("SAMPLE_NO" == "SAMPLE_NUMBER", "FISH_NO" == "FISH_SEQUENCE_NUMBER"))
   #---------------------------------------------------------------------
 
 PdataAge = Pdata #set up for age comps later
