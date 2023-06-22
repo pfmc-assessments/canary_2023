8284d9fbd6434a365249cc66856eabdd65f7bd9d
brianlangseth-NOAA <brian.langseth@NOAA.gov>
2023-06-22 09:10:58 -0700

    update historical comparison

--- code/compare historical assessments.R
+++ code/compare historical assessments.R
@@ -5,19 +5,102 @@
 #https://github.com/iantaylor-NOAA/YTRK_doc/blob/master/Rcode/historical_assessment_timeseries.R#L17
 #####################
 
+library(r4ss)
+
+setwd("\\\\nwcfile/FRAM/Assessments/Archives/CanaryRF")
+
+##
+#Read in old report files where I can from updates and full assessments
+##
+
+pp_2015 <- SS_output("CanaryRf_2015\\post-SSC base model")
+pp_2015 <- pp_2015$timeseries[,c("Yr","Area","Bio_all","Bio_smry","SpawnBio","Recruit_0")]
+
+pp_2011 <- SS_output("CanaryRf_2011\\Canary base case model files")
+pp_2011 <- pp_2011$timeseries[,c("Yr","Bio_all","Bio_smry","SpawnBio","Recruit_0")]
+
+####
+#Copy and paste files from before 2009. All from time series part of the report
+#Capabilities for reading in 2007 and 2005 
+####
+
+#2009: CanaryRf_2009\\base case model files Summary biomass at age 5
+#2007: CanaryRf_2007\\ss2.rep Summary biomass at age 5
+#2005: CanaryRf_2005\\Canary Models_2005\\ss2.rep Summary biomass at age 3
+
+#2009 Smy biomass at age 5
+rep_2009 <- file.path("CanaryRf_2009\\base case model files\\Report.sso")
+lines2009 <- readLines(rep_2009)
+
+pp_2009 <- read.table(rep_2009,
+                      skip = grep("TIME_SERIES", lines2009)[2],
+                      nrows = grep("SPR_series", lines2009)[2] - grep("TIME_SERIES", lines2009)[2] - 3,
+                      header = TRUE,
+                      fill = TRUE)[,c(2,3,5:8)]
+
+#2007 Smy biomass at age 5
+# rep_2007 <- file.path("CanaryRf_2007\\ss2.rep")
+# lines2007 <- readLines(rep_2007)
+# 
+# pp_2007 <- read.table(rep_2007,
+#                       skip = grep("TIME_SERIES", lines2007),
+#                       nrows = grep("SPR_series", lines2007) - grep("TIME_SERIES", lines2007) - 3,
+#                       header = TRUE,
+#                       fill = TRUE)[,c(2,3,5:8)]
+
+#2005 Smy biomass at age 3
+# rep_2005 <- file.path("CanaryRf_2005\\Canary Models_2005\\ss2.rep")
+# lines2005 <- readLines(rep_2005)
+# 
+# pp_2005 <- read.table(rep_2005,
+#                       skip = grep("TIME_SERIES", lines2005),
+#                       nrows = grep("SPR_series", lines2005) - grep("TIME_SERIES", lines2005) - 3,
+#                       header = TRUE,
+#                       fill = TRUE)[,c(2:7)]
+
+
+####
+#Copy and paste files from excel sheets - cant get readlines to work
+####
+
+#2002: CanaryRf_2002\\CanaryRf_2002_Models\\model\\canary2002-summary-output.csv
+#1999: CanaryRf_1999\CanaryRf_1999_Model_Crone\Canary\Docs\Result_displays.xls Scenario 1
+
+#2002 Smy biomass at age 3 - Doesnt work well
+# rep_2002 <- file.path("CanaryRf_2002\\CanaryRf_2002_Models\\model\\canary2002-summary-output.csv")
+# lines2002 <- readLines(rep_2002)
+# 
+# pp_2002 <- read.table(rep_2002,
+#                       skip = grep(" SUM-BIO IS FOR AGES  3 -  25  IN PERIOD   1", lines2002) + 6,
+#                       nrows = 60,
+#                       header = TRUE,
+#                       fill = TRUE)[,c(2:7)]
+
+
+##
+#Only for Oregon and Washington
+##
+
+#1996: Recognize text on tables 14 (scenario 1 M constant) and 15 (scenario 2 M age varying) in CanaryRf_1996\Canary rockfish.1996.SAFE.pdf
+#1994: Recognize text on tables 21 (scenario 1 M constant) and 22 (scenario 2 M age varying) in CanaryRf_1996\Canary rockfish.1996.SAFE.pdf
 
 
 
-# define directory on a specific computer
-if (system("hostname", intern=TRUE) %in% c("NWCLW04223033") ){
-  YTdir <- "C:/SS/Yellowtail/Yellowtail2017"
-  YTdir.doc <- file.path(YTdir, "YTRK_doc")
-  setwd(YTdir.doc)
-  # load models
-  if(!exists("mod1")){
-    load("r4ss/SS_output.RData")
-  }
-}
+####
+#Copied values are stored
+####
+historical_values <- read.csv(here('literature','previous assessments','historical_biomass_comparison.csv'), header = TRUE)
+
+
+
+
+
+plot(model$timeseries$Yr, model$timeseries$Bio_smry, 
+     type = 'l', lwd = 3, ylim = c(0, 5e4), yaxs = 'i')
+lines(ts2005$year[ts2005$season == 1], ts2005$bio.smry[ts2005$season == 1], col = 2, lwd = 3)
+
+
+
 
 totcatch <- aggregate(mod1$catch$kill_bio, by=list(mod1$catch$Yr), FUN=sum)
 names(totcatch) <- c("Yr","kill_bio")
@@ -34,7 +117,7 @@ assess.colors <- rich.colors.short(8)
 stocks2 <- stocks2[-1, ]
 # empty plot
 plot(0, type='n', xlim=c(1940, 2018), ylim=c(0, 200000),
-     axes=FALSE, xaxs='i', yaxs='i', xlab="Year", ylab="Age 4+ biomass (x1000 mt)")
+     axes=FALSE, xaxs='i', yaxs='i', xlab="Year", ylab="Age 5+ biomass (x1000 mt)")
 axis(1)
 axis(1, at=2016, label=2016)
 axis(2, at=pretty(c(0,200000)), lab=pretty(c(0,200000))/1000, las=1)
@@ -45,16 +128,7 @@ for(istock in 1:6){
           col=assess.colors[1+istock], type='l', lty=1,
           lwd=2, add=TRUE)
 }
-# read data-moderate results
-DMresults <- read.csv('./txt_files/age4plus_bio_from_Yellowtail_2013_DataModerate.csv')
-# subset to remove forecast
-DMresults <- DMresults[DMresults$Yr <= 2013,]
-# add polygon showing uncertainty
-## polygon(x=c(DMresults$Yr, rev(DMresults$Yr)),
-##         y=c(DMresults$Age4plus_bio.025, rev(DMresults$Age4plus_bio.975)),
-##         col=gray(0, alpha=0.1), border=NA)
-# add median data-moderate estimates
-lines(x=DMresults$Yr, DMresults$Age4plus_bio.median, col=assess.colors[8], lwd=2)
+
 
 # add current model estimate
 lines(mod1$timeseries[mod1$timeseries$Yr <= 2017, c("Yr","Bio_smry")],
